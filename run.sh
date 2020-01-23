3b37036aaf1e70f7b7d5e753d9d85e25882277ad
Vadim Igoshev <innovationnews@gmail.com>
2020-01-23 17:42:26 +0500

    skip old init

--- run.sh
+++ run.sh
@@ -1,15 +1,10 @@
 #!/bin/bash
-
-
 echo "Setting timezone"
 TZ=${TZ:-"Etc/UTC"}
-
 rm /etc/localtime
 ln -snf /usr/share/zoneinfo/$TZ /etc/localtime
 dpkg-reconfigure -f noninteractive tzdata
-
 cd /opt/firebird/bin
-
-./fbguard 
-
-
+#service xinetd start
+/etc/init.d/xinetd start 
+sh
